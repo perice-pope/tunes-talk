917acb4689bdf7d7337c0b40842ee5358ba44801
Perice Pope <perice09@gmail.com>
2019-08-26 11:18:35 -0700

    fix schema & file structure

--- models/post.js
+++ models/post.js
@@ -2,26 +2,27 @@ var mongoose = require('mongoose');
 
 // The postSchema is used to embedded docs in a user doc? 
 
+var commentSchema = new mongoose.Schema({
+  // user: { type:  Schema.Types.ObjectId, ref: 'User'},
+  //  commenterName: String,
+      text: String,    
+    });
+
 var postSchema = new mongoose.Schema({
   song: { title: String,
           artist: String, 
     },
-    comment: String
+    comments: [commentSchema]
 }, {
   timestamps: true
 });
-
-var commentSchema = new mongoose.Schema({
-// user: { type:  Schema.Types.ObjectId, ref: 'User'},
-//  commenterName: String,
-    comment: String,    
-  });
   
 var userSchema = new mongoose.Schema({
   name: String,
   email: String,
   avatar: String,
-  googleId: String
+  googleId: String, 
+  posts: [postSchema]
 }, {
   timestamps: true
 });
